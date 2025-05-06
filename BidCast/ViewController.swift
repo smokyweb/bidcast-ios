8fdc42d9b3a8519dd9f9c10e7fe2fe13ea80c966
Sr. iOS Developer <digvijayiosdeveloper@gmail.com>
2025-05-06 11:29:50 +0530

    Initial Commit

---
+++ BidCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BidCast
+//
+//  Created by Abdul-JAM-E-157 on 06/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BidCastTests/BidCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  BidCastTests.swift
+//  BidCastTests
+//
+//  Created by Abdul-JAM-E-157 on 06/05/25.
+//
+
+import Testing
+@testable import BidCast
+
+struct BidCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
