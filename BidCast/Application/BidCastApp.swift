09125755850d9c31e877700386bf665a1c96d078
ios <>
2025-07-11 15:37:22 +0530

    -Fixed Splash Screen White background issue.

--- BidCast/Application/BidCastApp.swift
+++ BidCast/Application/BidCastApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct BidCastApp: App {
+    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var appRootManager = AppRootManager()
     @State private var accountNavigationPath = NavigationPath()
--- BidCast/Screens/Common/SplashScreen/SplashScreen.swift
+++ BidCast/Screens/Common/SplashScreen/SplashScreen.swift
@@ -48,3 +48,4 @@ struct SplashScreen: View {
     SplashScreen()
 }
 
+
