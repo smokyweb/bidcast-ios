3059114de55ae4d5f85f03fca1211bdb18a87331
iOS-Developer <>
2025-07-22 10:31:19 +0530

    Fixed status seller

--- BidCast/Screens/Account/View/SellerHub/SellerStatus/View/SellerStatusScreen.swift
+++ BidCast/Screens/Account/View/SellerHub/SellerStatus/View/SellerStatusScreen.swift
@@ -44,10 +44,14 @@ struct SellerStatusScreen: View {
             // MARK: - Scrollable Content
             ScrollView {
                 VStack(spacing: 12) {
-                    let seller = sellerData.live_sell_vendor
-                    let market = sellerData.marketplace_vendor
-                    SellerStatusCardView(title: seller?.title ?? "", status: seller?.status ?? "", icon: "cart.fill", subtitle: seller?.submitted ?? "")
-                    SellerStatusCardView(title: market?.title ?? "", status: market?.status ?? "", icon: "video.fill", subtitle: market?.vendor_since ?? "")
+                    var seller = sellerData.live_sell_vendor
+                    var market = sellerData.marketplace_vendor
+                    if seller != nil {
+                        SellerStatusCardView(title: seller?.title ?? "", status: seller?.status ?? "", icon: "cart.fill", subtitle: seller?.submitted ?? "")
+                    }
+                    if market != nil {
+                        SellerStatusCardView(title: market?.title ?? "", status: market?.status ?? "", icon: "video.fill", subtitle: market?.vendor_since ?? "")
+                    }
 //
 //                    .padding(.horizontal, 16)
                     
