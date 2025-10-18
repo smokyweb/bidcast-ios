aa51d7b4e446967e7d6539575a8a30f321160f90
JamTech <admn@Vivek-JAME-328.local>
2025-10-18 16:15:48 +0530

    Show default image

--- BidCast/Screens/Common/CustomProfileImage.swift
+++ BidCast/Screens/Common/CustomProfileImage.swift
@@ -34,7 +34,7 @@ struct CustomProfileImage: View {
                     if url.contains("http") {
                         Image("defaultUser")
                             .resizable()
-                            .aspectRatio(contentMode: .fit)
+                            .aspectRatio(contentMode: .fill)
                             .frame(width: size, height: size)
                             .applyClip(isCircular: isCircular, cornerRadius: cornerRadius)
                             .shadow(color: Color.black.opacity(0.2), radius: 3, x: 0, y: 2)
@@ -54,7 +54,7 @@ struct CustomProfileImage: View {
         } else {
             Image("defaultUser")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: .fill)
                 .frame(width: size, height: size)
                 .applyClip(isCircular: isCircular, cornerRadius: cornerRadius)
                 .shadow(color: Color.black.opacity(0.1), radius: 3, x: 0, y: 2)
