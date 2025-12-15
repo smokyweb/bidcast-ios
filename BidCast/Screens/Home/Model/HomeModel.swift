fc0e30e1145b3801729d07b8cc1b574f6041b951
JamTech <admn@Vivek-JAME-328.local>
2025-12-15 19:21:58 +0530

    Updated Home Model

--- BidCast/Screens/Home/Model/HomeModel.swift
+++ BidCast/Screens/Home/Model/HomeModel.swift
@@ -12,22 +12,31 @@ struct HomeModel: Codable, Identifiable {
     var title: String?
     var date: String?
     var time: String?
+    var language: String?
+    var repeat_value: String?
+    var is_repeat: Bool?
+    var is_explicit: Bool?
     var user_id: Int?
+    var show_discoverability: String?
     var category_id: Int?
-    var product_ids: [String]? 
+    var product_ids: [String]?
     var auction_type_id: Int?
     var thumbnail: [String]?
+    var products : [ProductDataModel]?
     var img_thumbnail: [String]?
     var is_live: Bool?
-    var latest_viewer_count : Int?
     var viewer_count: Int?
-    var room_id: String?
-    var products : [ProductDataModel]?
-    var category: Category?
-    var user: User?
+    var latest_viewer_count: Int?
     var promoted_at: String?
     var rtc_token: String?
     var promote_show_id: Int?
     var started_at: String?
+    var share_count: Int?
+    var recording_resource_id: String?
+    var recording_sid: String?
+    var room_id: String?
+    var category: Category?
+    var user: User?
 }
+
 
