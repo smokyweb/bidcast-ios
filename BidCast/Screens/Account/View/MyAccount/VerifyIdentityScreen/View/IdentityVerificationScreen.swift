fab30e5a43eb56ae6a332f1359fe96c909175d50
iOS_Developer <>
2026-01-20 12:15:11 +0530

    Fixed the UI of kyc

--- BidCast/Screens/Account/View/MyAccount/VerifyIdentityScreen/View/IdentityVerificationScreen.swift
+++ BidCast/Screens/Account/View/MyAccount/VerifyIdentityScreen/View/IdentityVerificationScreen.swift
@@ -31,12 +31,16 @@ struct IdentityVerificationScreen: View {
         secondaryButtonTitle: nil,
         showButtons: true
     )
+    
+    @State private var showKycWebView = false
+    @State private var kycURL: URL?
+    @State private var didOpenKyc = false
     var body: some View {
         VStack(spacing: 0) {
 
             // Header
             PrimaryHeader(
-                title: "Add KYC",
+                title: kycURL == nil ? "Update KYC" : "Add KYC",
                 leadingImgArr: ["chevron.left"],
                 onClickLeading: { _ in
                     presentationMode.wrappedValue.dismiss()
@@ -44,13 +48,28 @@ struct IdentityVerificationScreen: View {
                 count: .constant(0)
             )
             .background(Color.white)
-            ScrollView{
-                VStack(spacing: 16){
-                    contentView
+//            ScrollView{
+//                VStack(spacing: 16){
+//                    contentView
+//                }
+//            }
+//            .padding(.top,24)
+//            .padding(.horizontal,16)
+            Group {
+                if let url = kycURL {
+                    KycWebView(url: url) {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                } else {
+                    ScrollView{
+                        VStack(spacing: 16){
+                            contentView
+                        }
+                    }
+                    .padding(.horizontal,16)
                 }
             }
-            .padding(.top,24)
-            .padding(.horizontal,16)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
        
         .background(.backGround)
@@ -65,55 +84,74 @@ struct IdentityVerificationScreen: View {
     // MARK: - Content View
     @ViewBuilder
     private var contentView: some View {
-        Text("Identity Verification")
-            .font(.custom(poppinsBold, size: 24))
-            .foregroundColor(.black)
-        
-        HStack{
-            Text("KYC Status")
-                .font(.custom(poppinsRegular, size: 16))
-                .foregroundColor(.darkGray)
-            Spacer()
-            Text(kycData.kycStatus?.capitalizingFirstLetter() ?? "")
-                .font(.custom(poppinsSemiBold, size: 16))
-                .foregroundColor(.defaultTheme)
-        }
-        
-        if kycData.kycStatus == "inactive" {
-            inactiveKycView
-        } else {
-            activeKycView
-        }
-    }
 
-    // MARK: - Inactive KYC View
-    private var inactiveKycView: some View {
-        VStack(spacing: 12) {
+//        Text("Identity Verification")
+//            .font(.custom(poppinsBold, size: 24))
+//            .foregroundColor(.black)
+//
+//        HStack {
+//            Text("KYC Status")
+//                .font(.custom(poppinsRegular, size: 16))
+//                .foregroundColor(.darkGray)
+//
+//            Spacer()
+//
+//            Text(kycData.kycStatus?.capitalizingFirstLetter() ?? "")
+//                .font(.custom(poppinsSemiBold, size: 16))
+//                .foregroundColor(.success)
+//        }
 
-            Text("Complete your KYC to continue")
-                .font(.custom(poppinsRegular, size: 16))
-                .foregroundColor(.gray)
+        if kycData.kycStatus == "active" {
 
-            PrimaryButton(title: "Start KYC",onButtonClick: {
+            // 🔹 Profile header
+            KycProfileHeaderView(
+                profileImageURL: UserDefaults.profileURL, fullName: UserDefaults.fullName,
+                username: UserDefaults.userName.capitalizingFirstLetter() ,
+                status: kycData.kycStatus ?? ""
+            )
+
+            // 🔹 KYC details
+            if let details = kycData.kycDetails {
+                KycDetailsView(details: details)
+            }
+            
+            PrimaryButton(title: "Update KYC",onButtonClick: {
                 if let urlString = viewModel.checkKycDict.data?.link,
                    let url = URL(string: urlString) {
                     openURL(url)
                 }
-            }).padding(.horizontal, 16)
+            })
         }
-        .padding(.top, 40)
     }
 
-    // MARK: - Active / Completed KYC View
-    private var activeKycView: some View {
-            VStack(spacing: 12) {
-               
-                Text("Your KYC has been completed.")
-                    .font(.custom(poppinsRegular, size: 16))
-                    .foregroundColor(.success)
-            }
-        
-    }
+//    // MARK: - Inactive KYC View
+//    private var inactiveKycView: some View {
+//        VStack(spacing: 12) {
+//
+//            Text("Complete your KYC to continue")
+//                .font(.custom(poppinsRegular, size: 16))
+//                .foregroundColor(.gray)
+//
+//            PrimaryButton(title: "Start KYC",onButtonClick: {
+//                if let urlString = viewModel.checkKycDict.data?.link,
+//                   let url = URL(string: urlString) {
+//                    openURL(url)
+//                }
+//            }).padding(.horizontal, 16)
+//        }
+//        .padding(.top, 40)
+//    }
+//
+//    // MARK: - Active / Completed KYC View
+//    private var activeKycView: some View {
+//            VStack(spacing: 12) {
+//               
+//                Text("Your KYC has been completed.")
+//                    .font(.custom(poppinsRegular, size: 16))
+//                    .foregroundColor(.success)
+//            }
+//        
+//    }
 
     
     private func loadKycStatus() {
@@ -139,22 +177,207 @@ struct IdentityVerificationScreen: View {
             ) {
                
                 try await viewModel.checkKycDetail()
-                try await viewModel.getKycDetail()
+//                try await viewModel.getKycDetail()
                 
             }
         }
     }
 
-    func success(){
+    func success() {
         let response = viewModel.checkKycDict
-        if response.status == "success"{
-            kycData = response.data ?? CheckKycModel()
+
+        guard response.status == "success" else { return }
+
+        // ✅ Always store KYC data
+        kycData = response.data ?? CheckKycModel()
+
+        // 🚀 If inactive → open web view
+        if kycData.kycStatus == "inactive",
+           let link = kycData.link,
+           let url = URL(string: link) {
+
+            kycURL = url
+        }
+
+        // ✅ If active → show content view only
+        if kycData.kycStatus == "active" {
+            kycURL = nil
         }
     }
+
+
     func detailSuccess(){
         let response = viewModel.kycDetailsDict
         if response.status == "success"{
 //            kycData = response.data ?? KycDetailsModel()
         }
+    }
+}
+
+import SwiftUI
+import WebKit
+import SVProgressHUD
+
+struct KycWebView: UIViewRepresentable {
+
+    let url: URL
+    let onKycCompleted: () -> Void
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onKycCompleted: onKycCompleted)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+
+        // 🔄 Start loader
+        DispatchQueue.main.async {
+            SVProgressHUD.show()
+        }
+
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    final class Coordinator: NSObject, WKNavigationDelegate {
+
+        let onKycCompleted: () -> Void
+
+        init(onKycCompleted: @escaping () -> Void) {
+            self.onKycCompleted = onKycCompleted
+        }
+
+        // 🔄 Page started loading
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            SVProgressHUD.show()
+        }
+
+        // ✅ Page loaded successfully
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            SVProgressHUD.dismiss()
+        }
+
+        // ❌ Page failed
+        func webView(
+            _ webView: WKWebView,
+            didFail navigation: WKNavigation!,
+            withError error: Error
+        ) {
+            SVProgressHUD.dismiss()
+        }
+
+        func webView(
+            _ webView: WKWebView,
+            didFailProvisionalNavigation navigation: WKNavigation!,
+            withError error: Error
+        ) {
+            SVProgressHUD.dismiss()
+        }
+
+        // 🔍 Detect KYC completion redirect
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        ) {
+
+            if let urlString = navigationAction.request.url?.absoluteString {
+                if urlString.contains("kyc-success") ||
+                   urlString.contains("completed") {
+
+                    SVProgressHUD.dismiss()
+                    onKycCompleted()
+                }
+            }
+
+            decisionHandler(.allow)
+        }
+    }
+}
+
+struct KycDetailsView: View {
+    let details: KycDetailsModel
+
+    var body: some View {
+        VStack(spacing: 0) {
+
+            KycDetailRow(title: "Phone", value: details.phone)
+            Divider()
+
+            KycDetailRow(title: "City", value: details.city)
+            Divider()
+
+            KycDetailRow(title: "Country", value: details.country)
+            Divider()
+
+            KycDetailRow(title: "Postal Code", value: details.postalCode)
+            Divider()
+
+            KycDetailRow(title: "Bank ID", value: details.bankID)
+            Divider()
+
+            KycDetailRow(title: "Routing Number", value: details.rountingNumber)
+            Divider()
+
+            KycDetailRow(title: "Currency", value: details.currency)
+        }
+        .padding(16)
+        .background(Color.white)
+        .cornerRadius(12)
+    }
+}
+struct KycDetailRow: View {
+    let title: String
+    let value: String?
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.custom(poppinsRegular, size: 14))
+                .foregroundColor(.darkGray)
+
+            Spacer()
+
+            Text(value ?? "-")
+                .font(.custom(poppinsMedium, size: 14))
+                .foregroundColor(.black)
+        }
+        .padding(.vertical, 10)
+    }
+}
+struct KycProfileHeaderView: View {
+    let profileImageURL: String?
+    let fullName : String
+    let username: String
+    let status: String
+
+    var body: some View {
+        VStack(spacing: 12) {
+CustomProfileImage(url: profileImageURL ?? "", isCircular: true, size: 80)
+//            AsyncImage(url: URL(string: profileImageURL ?? "")) { image in
+//                image
+//                    .resizable()
+//                    .scaledToFill()
+//            } placeholder: {
+//                Circle()
+//                    .fill(Color.gray.opacity(0.3))
+//            }
+//            .frame(width: 80, height: 80)
+//            .clipShape(Circle())
+            Text(fullName.capitalizingFirstLetter())
+                .font(.custom(poppinsSemiBold, size: 18))
+                .foregroundColor(.black)
+            Text(username)
+                .font(.custom(poppinsSemiBold, size: 14))
+                .foregroundColor(.darkGray)
+
+            Text(status.capitalizingFirstLetter())
+                .font(.custom(poppinsRegular, size: 14))
+                .foregroundColor(.defaultTheme)
+        }
+        .padding(.top, 16)
     }
 }
